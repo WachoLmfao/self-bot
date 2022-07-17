e0f010a754add8215ba9ca8c745855cb69eae485
Justunk <105544609+WachoLmfao@users.noreply.github.com>
2022-07-17 13:47:45 +0300

    Update self-bot.py

--- self-bot.py
+++ self-bot.py
@@ -1,21 +1,43 @@
 import discord
 from discord.ext import commands
-import time
-from random import randint
+import asyncio
 
-TOKEN = "TOKEN_HERE" 
+bot = commands.Bot(command_prefix="1") #this is self-bots prefix, feel free to change it anytime
 
+async def status_task():
+    while True:
+     await bot.change_presence(activity=discord.Game('Counter-Strike Global Offensive'))
+     await asyncio.sleep(2)
+     await bot.change_presence(activity=discord.Game('Minecraft'))
+     await asyncio.sleep(2)
+     await bot.change_presence(activity=discord.Game('Among Us'))
+     await asyncio.sleep(2)
+     await bot.change_presence(activity=discord.Game('The LEGO® Movie™ - Videogame'))
+     await asyncio.sleep(2)
+     await bot.change_presence(activity=discord.Game('Grand Theft Auto V'))
+     await asyncio.sleep(2)
+     await bot.change_presence(activity=discord.Game('Forza Horizon 5'))
+     await asyncio.sleep(2)
+     await bot.change_presence(activity=discord.Game('Grand Theft Auto IV'))
+     await asyncio.sleep(2)
+     await bot.change_presence(activity=discord.Game('Spider-Man 3'))
+    
+    #this command right here, changes your game every 2 seconds
 
-client = discord.Client()
-client = commands.Bot(command_prefix="change_the_prefix_here") #change the prefix in here
-
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("Bot is ready!") #this prints "Bot is ready!" when its on
+    bot.loop.create_task(status_task()) #and this loops the status_task
     
-    
- #Do stuff here 
-    
-client.run(TOKEN)
 
-#dont forget to type "pip install -U discord.py-self" in the console, otherwise it wont work
+@bot.event
+async def on_message_delete(message):
+  print(f"Message deleted: {message.content} by {message.author}") #When a message is deleted, it prints its content and author
+
+@bot.command()
+async def devtab(ctx):
+  await ctx.send("https://replit.com/@justunk/py#main.py") #devtab
+
+bot.run("TOKEN_HERE") #and place your self bots token here
+
+#also type "pip install -U discord.py-self" in the console before launching it !
